1f55f6e1499df8b1d5acac65e06de5bb187aa6ae
LITSOU <59511848+litsou@users.noreply.github.com>
2024-08-15 15:43:04 +0800

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+export default function Footer() {
+  return (
+    <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
+      <div >
+        <p className="text-xs text-gray-500">Copyright Ⓒ 2024 All rights reserved. 請勿上傳違反法律的圖片，違者後果自負。 本程式基於Cloudflare Pages，開源自於
+          <Link 
+          href="https://github.com/x-dr/telegraph-Image"
+          className="text-blue-300  hover:text-red-900 ml-1"
+          target="_blank"
+          rel="noopener noreferrer"
+          >GitHub Telegraph-Image</Link> </p>
+      </div>
+    </footer>
+  );
+}
